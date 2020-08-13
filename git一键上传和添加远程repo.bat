7d5ec851f59a8846d6deee2d61c5bc9622923182
fwx-gitrepo3-test5 <fwx5618177@gmail.com>
2020-08-13 23:56:32 +0900

    modify:  add: ~,-156:32.44

--- git一键上传和添加远程repo.bat
+++ git一键上传和添加远程repo.bat
@@ -91,9 +91,66 @@ set repoName=
 set branch=
 
 echo 全自动上传
-set /p str=文件更改信息：
+set /p str=全部文件更改信息记录：
+
+::得到目录数据
+for /f "tokens=4 delims=\ " %%i in ('dir ^| findstr /c:"%date:~0,10%" ^| findstr /v /c:"\."') do (
+	echo %%i>>list.txt
+)
+
+::得到文件的新增数据
+for /f %%i in (list.txt) do (
+	
+	cd %%i
+
+	::获取新增文件名
+	for /f "tokens=4 delims=\ " %%a in ('dir ^| findstr /c:"%date:~0,10%" ^| 	findstr /v /c:"DIR"') do (
+		echo %%a >> ..\fileList.txt
+	) 
+	cd ..
+)
+
+
+::git add .
+::逐个文件夹增加
+for /f %%i in (list.txt) do (
+	git add %%i
+	cd %%i
+	::获取新增文件名
+	::git commit -am "%str%"
+	::添加修改信息
+	for /f "tokens=4 delims=\ " %%a in ('dir ^| findstr /c:"%date:~0,10%" ^| findstr /v /c:"DIR"') do (
+		echo %%a
+		git commit -m "add func: %%a in %date:~0,10%-%time%" %%a
+	)
+	cd ..
+	
+)
+
+type list.txt >> UploadLog.txt
+echo %date:~0,10%-%time% >> UploadLog.txt
+echo. >> UploadLog.txt
+type fileList.txt >> UploadLog.txt
+echo %date:~0,10%-%time% >> UploadLog.txt
+echo. >> UploadLog.txt
+
+::获取更改名单，一行显示
+
+for /f %%i in (fileList.txt) do (
+	set /p="%%i,"<nul>>tmplist.txt
+)
+
+for /f %%i in (tmplist.txt) do (
+	set tmpStr=%%i
+)
+
+echo !tmpStr:~,-1!
+
+del list.txt fileList.txt tmplist.txt
+
+
 git add .
-git commit -am "%str%"
+git commit -am "modify: %str% add: !tmpStr:~,-1! in %date:~0,10%-%time% "
 
 for /f %%i in ('git remote') do (
 	echo 远程仓库名字：%%i
@@ -112,7 +169,7 @@ git push !repoName! !branch!
 
 echo.
 echo 上传完成
-pause
+pause > nul
 goto start
 
 :s0
